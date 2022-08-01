6ee18344f94cfd646479d84294033ab80d4ef7b3
HIRANI MADHAV RAJESHBHAI <hiranimadhavrajeshbhai@HIRANIs-MacBook-Pro.local>
2022-08-01 17:45:14 +0530

    C++ basic programs

---
+++ c++_basic_program/p1maxbetweentwo.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int a,b;
+    cout<<endl<<"Enter a=>";
+    cin>>a;
+
+    cout<<endl<<"Enter b=>";
+    cin>>b;
+    
+
+    if(a>b)
+    {
+        cout<<endl<<a<<" is greater";
+    }
+    
+    else
+    {
+        cout<<endl<<b<<" is greater";
+    }
+
+return 0;
+}
