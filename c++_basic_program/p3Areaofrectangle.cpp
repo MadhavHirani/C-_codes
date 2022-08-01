e42e67723b9a2c35b4c09062560c6f3c2ad7a888
Madhav <mrhirani24@gmail.com>
2022-08-01 18:00:57 +0530

    Area of rectangle

---
+++ c++_basic_program/p3Areaofrectangle.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,area;
+    cout<<endl<<"Enter a";
+    cin>>a;
+
+    cout<<endl<<"Enter b";
+    cin>>b;
+
+
+    cout<<endl<<"Area of rectangle"<<a*b;
+    return 0;
+}
