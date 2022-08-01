2e265f6199a416fbda2b0adacf03225409bd473c
Madhav <mrhirani24@gmail.com>
2022-08-01 18:03:52 +0530

    Simple Interest

---
+++ c++_basic_program/p4Simpleinterest.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int p,r,t,si;
+    cout<<endl<<"Enter pricipal amount";
+    cin>>p;
+
+    cout<<endl<<"Enter rate of interest";
+    cin>>r;
+
+    cout<<endl<<"Enter time taken";
+    cin>>t;
+
+     cout<<endl<<"si"<<(p*r*t)/100;
+     return 0;
+}
